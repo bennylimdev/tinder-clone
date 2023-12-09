226eee73c5215bb753ad05d6f200c57f5b2f92eb
Ben Lim <lucakingoff@gmail.com>
2023-12-09 07:03:00 -0500

    react version fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './Header';
+import TinderCards from './TinderCards';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Header />
+      <Header />
+      <Router>
+      <Routes>
+        <Route path="/" element={<TinderCards />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import TinderCard from "react-tinder-card";
+
+function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+        name: 'steve jobs',
+        url: 'https://cdn.profoto.com/cdn/053149e/contentassets/d39349344d004f9b8963df1551f24bf4/profoto-albert-watson-steve-jobs-pinned-image-original.jpg?width=1280&quality=75&format=jpg'
+        },
+        {
+        name: 'bob the builder',
+        url: 'https://m.media-amazon.com/images/M/MV5BNjRlYjgwMWMtNDFmMy00OWQ0LWFhMTMtNWE3MTU4ZjQ3MjgyXkEyXkFqcGdeQXVyNzU1NzE3NTg@._V1_QL75_UX500_CR0,47,500,281_.jpg'    
+        }
+]);
+
+    return (
+        <div>
+            <h1>Tinder Cards</h1>
+
+            {people.map(person => (
+                <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={['up', 'down']}
+                >
+                 <div
+                 style={{ backgroundImage: 'url(${person.url})' }} 
+                 className="card"
+                 >
+                    <h3>{person.name}</h3>
+                 </div>
+                </TinderCard>
+            ))}
+        </div>
+    )
+}
+
+export default TinderCards;
